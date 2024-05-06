8f651f864bd61c44c18e6af44c4026b69665c645
WeeHorse <benjaminberglund@gmail.com>
2024-05-06 10:02:38 +0200

    added crazy attempt at connecting myssql in minimal api and outputting some data for no good reason

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,12 @@
 using Microsoft.Extensions.FileProviders;
 using System.IO;
+using MySql.Data.MySqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// db
+State state = new("uid=root;pwd=;database=homenet;server=localhost;port=3307");
+builder.Services.AddSingleton(state);
 
 var app = builder.Build();
 
@@ -28,6 +33,27 @@ app.UseStaticFiles(new StaticFileOptions
     FileProvider = new PhysicalFileProvider(
         Path.Combine(Directory.GetCurrentDirectory(), "frontend")),
     RequestPath = ""
+});
+
+// routes
+
+app.MapGet("/", (State state) =>
+{
+    using var reader = MySqlHelper.ExecuteReader(
+    state.DB,
+    "SELECT ort FROM adresser LIMIT 1"
+    );
+
+    var ort = new Record(string ort);        
+
+    if (reader.Read())
+    {
+        return reader.GetString("ort");
+    }
+    else
+    {
+        return "whatever";
+    }
 });
 
 
